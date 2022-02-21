db51f093948a5a67ccbd0ea1729007be22738561
kelvin-omoh <95302653+kelvin-omoh@users.noreply.github.com>
2022-02-21 13:02:42 +0100

    Add files via upload

---
+++ currency converter/currency converter.js
@@ -0,0 +1,11 @@
+      function subm(){
+    var a, b,c;
+    n=512;
+    n=Number(document.getElementById("naira-input").innerHTML("").value);
+    a=Number(document.getElementById("franc-input").innerHTML("").value);
+    b=Number(document.getElementById("pound-input").value);
+    c=Number(document.getElementById("dollar-input").value);
+    a=a/512;
+    b=b/565.35;
+
+}
